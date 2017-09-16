50de7e52189ae2b3b2282b23369efe221b677fb5
Lucas Serven <lserven@gmail.com>
2017-09-16 15:36:34 -0700

    1.7.3-tectonic.2

---
+++ main.tf
@@ -0,0 +1,171 @@
+provider "aws" {
+  region = "${var.tectonic_aws_region}"
+}
+
+data "aws_availability_zones" "azs" {}
+
+module "vpc" {
+  source = "github.com/coreos/tectonic-installer//modules/aws/vpc?ref=1.7.3-tectonic.2"
+
+  cidr_block   = "${var.tectonic_aws_vpc_cidr_block}"
+  cluster_name = "${var.tectonic_cluster_name}"
+
+  external_vpc_id         = "${var.tectonic_aws_external_vpc_id}"
+  external_master_subnets = "${compact(var.tectonic_aws_external_master_subnet_ids)}"
+  external_worker_subnets = "${compact(var.tectonic_aws_external_worker_subnet_ids)}"
+  cluster_id              = "${module.tectonic.cluster_id}"
+  extra_tags              = "${var.tectonic_aws_extra_tags}"
+  enable_etcd_sg          = "${!var.tectonic_experimental && length(compact(var.tectonic_etcd_servers)) == 0 ? 1 : 0}"
+
+  # VPC layout settings.
+  #
+  # The following parameters control the layout of the VPC accross availability zones.
+  # Two modes are available:
+  # A. Explicitly configure a list of AZs + associated subnet CIDRs
+  # B. Let the module calculate subnets accross a set number of AZs
+  #
+  # To enable mode A, configure a set of AZs + CIDRs for masters and workers using the
+  # "tectonic_aws_master_custom_subnets" and "tectonic_aws_worker_custom_subnets" variables.
+  #
+  # To enable mode B, make sure that "tectonic_aws_master_custom_subnets" and "tectonic_aws_worker_custom_subnets" 
+  # ARE NOT SET.
+
+  # These counts could be deducted by length(keys(var.tectonic_aws_master_custom_subnets)) 
+  # but there is a restriction on passing computed values as counts. This approach works around that.
+  master_az_count = "${length(keys(var.tectonic_aws_master_custom_subnets)) > 0 ? "${length(keys(var.tectonic_aws_master_custom_subnets))}" : "${length(data.aws_availability_zones.azs.names)}"}"
+  worker_az_count = "${length(keys(var.tectonic_aws_worker_custom_subnets)) > 0 ? "${length(keys(var.tectonic_aws_worker_custom_subnets))}" : "${length(data.aws_availability_zones.azs.names)}"}"
+  # The appending of the "padding" element is required as workaround since the function
+  # element() won't work on empty lists. See https://github.com/hashicorp/terraform/issues/11210
+  master_subnets = "${concat(values(var.tectonic_aws_master_custom_subnets),list("padding"))}"
+  worker_subnets = "${concat(values(var.tectonic_aws_worker_custom_subnets),list("padding"))}"
+  # The split() / join() trick works around the limitation of ternary operator expressions 
+  # only being able to return strings.
+  master_azs = "${ split("|", "${length(keys(var.tectonic_aws_master_custom_subnets))}" > 0 ?
+    join("|", keys(var.tectonic_aws_master_custom_subnets)) :
+    join("|", data.aws_availability_zones.azs.names)
+  )}"
+  worker_azs = "${ split("|", "${length(keys(var.tectonic_aws_worker_custom_subnets))}" > 0 ?
+    join("|", keys(var.tectonic_aws_worker_custom_subnets)) :
+    join("|", data.aws_availability_zones.azs.names)
+  )}"
+}
+
+module "etcd" {
+  source = "github.com/coreos/tectonic-installer//modules/aws/etcd?ref=1.7.3-tectonic.2"
+
+  instance_count = "${var.tectonic_experimental ? 0 : var.tectonic_etcd_count > 0 ? var.tectonic_etcd_count : length(data.aws_availability_zones.azs.names) == 5 ? 5 : 3}"
+  ec2_type       = "${var.tectonic_aws_etcd_ec2_type}"
+  sg_ids         = "${concat(var.tectonic_aws_etcd_extra_sg_ids, list(module.vpc.etcd_sg_id))}"
+
+  ssh_key         = "${var.tectonic_aws_ssh_key}"
+  cl_channel      = "${var.tectonic_cl_channel}"
+  container_image = "${var.tectonic_container_images["etcd"]}"
+
+  subnets = "${module.vpc.worker_subnet_ids}"
+
+  dns_zone_id  = "${var.tectonic_aws_external_private_zone == "" ? join("", aws_route53_zone.tectonic_int.*.zone_id) : var.tectonic_aws_external_private_zone}"
+  base_domain  = "${var.tectonic_base_domain}"
+  cluster_name = "${var.tectonic_cluster_name}"
+
+  external_endpoints = "${compact(var.tectonic_etcd_servers)}"
+  cluster_id         = "${module.tectonic.cluster_id}"
+  extra_tags         = "${var.tectonic_aws_extra_tags}"
+
+  root_volume_type = "${var.tectonic_aws_etcd_root_volume_type}"
+  root_volume_size = "${var.tectonic_aws_etcd_root_volume_size}"
+  root_volume_iops = "${var.tectonic_aws_etcd_root_volume_iops}"
+
+  dns_enabled = "${!var.tectonic_experimental && length(compact(var.tectonic_etcd_servers)) == 0}"
+  tls_enabled = "${var.tectonic_etcd_tls_enabled}"
+
+  tls_zip = "${module.bootkube.etcd_tls_zip}"
+}
+
+module "ignition_masters" {
+  source = "github.com/coreos/tectonic-installer//modules/aws/ignition?ref=1.7.3-tectonic.2"
+
+  kubelet_node_label        = "node-role.kubernetes.io/master"
+  kubelet_node_taints       = "node-role.kubernetes.io/master=:NoSchedule"
+  kubelet_cni_bin_dir       = "${var.tectonic_calico_network_policy ? "/var/lib/cni/bin" : "" }"
+  kube_dns_service_ip       = "${module.bootkube.kube_dns_service_ip}"
+  kubeconfig_s3_location    = "${aws_s3_bucket_object.kubeconfig.bucket}/${aws_s3_bucket_object.kubeconfig.key}"
+  assets_s3_location        = "${aws_s3_bucket_object.tectonic_assets.bucket}/${aws_s3_bucket_object.tectonic_assets.key}"
+  container_images          = "${var.tectonic_container_images}"
+  bootkube_service          = "${module.bootkube.systemd_service}"
+  tectonic_service          = "${module.tectonic.systemd_service}"
+  tectonic_service_disabled = "${var.tectonic_vanilla_k8s}"
+  cluster_name              = "${var.tectonic_cluster_name}"
+  image_re                  = "${var.tectonic_image_re}"
+}
+
+module "masters" {
+  source = "github.com/coreos/tectonic-installer//modules/aws/master-asg?ref=1.7.3-tectonic.2"
+
+  instance_count  = "${var.tectonic_master_count}"
+  ec2_type        = "${var.tectonic_aws_master_ec2_type}"
+  cluster_name    = "${var.tectonic_cluster_name}"
+  master_iam_role = "${var.tectonic_aws_master_iam_role_name}"
+
+  subnet_ids = "${module.vpc.master_subnet_ids}"
+
+  master_sg_ids  = "${concat(var.tectonic_aws_master_extra_sg_ids, list(module.vpc.master_sg_id))}"
+  api_sg_ids     = ["${module.vpc.api_sg_id}"]
+  console_sg_ids = ["${module.vpc.console_sg_id}"]
+
+  ssh_key    = "${var.tectonic_aws_ssh_key}"
+  cl_channel = "${var.tectonic_cl_channel}"
+  user_data  = "${module.ignition_masters.ignition}"
+
+  internal_zone_id             = "${var.tectonic_aws_external_private_zone == "" ? join("", aws_route53_zone.tectonic_int.*.zone_id) : var.tectonic_aws_external_private_zone}"
+  external_zone_id             = "${join("", data.aws_route53_zone.tectonic_ext.*.zone_id)}"
+  base_domain                  = "${var.tectonic_base_domain}"
+  public_vpc                   = "${var.tectonic_aws_external_vpc_public}"
+  cluster_id                   = "${module.tectonic.cluster_id}"
+  extra_tags                   = "${var.tectonic_aws_extra_tags}"
+  autoscaling_group_extra_tags = "${var.tectonic_autoscaling_group_extra_tags}"
+  custom_dns_name              = "${var.tectonic_dns_name}"
+
+  root_volume_type = "${var.tectonic_aws_master_root_volume_type}"
+  root_volume_size = "${var.tectonic_aws_master_root_volume_size}"
+  root_volume_iops = "${var.tectonic_aws_master_root_volume_iops}"
+}
+
+module "ignition_workers" {
+  source = "github.com/coreos/tectonic-installer//modules/aws/ignition?ref=1.7.3-tectonic.2"
+
+  kubelet_node_label     = "node-role.kubernetes.io/node"
+  kubelet_node_taints    = ""
+  kubelet_cni_bin_dir    = "${var.tectonic_calico_network_policy ? "/var/lib/cni/bin" : "" }"
+  kube_dns_service_ip    = "${module.bootkube.kube_dns_service_ip}"
+  kubeconfig_s3_location = "${aws_s3_bucket_object.kubeconfig.bucket}/${aws_s3_bucket_object.kubeconfig.key}"
+  assets_s3_location     = ""
+  container_images       = "${var.tectonic_container_images}"
+  bootkube_service       = ""
+  tectonic_service       = ""
+  cluster_name           = ""
+  image_re               = "${var.tectonic_image_re}"
+}
+
+module "workers" {
+  source = "github.com/coreos/tectonic-installer//modules/aws/worker-asg?ref=1.7.3-tectonic.2"
+
+  instance_count  = "${var.tectonic_worker_count}"
+  ec2_type        = "${var.tectonic_aws_worker_ec2_type}"
+  cluster_name    = "${var.tectonic_cluster_name}"
+  worker_iam_role = "${var.tectonic_aws_worker_iam_role_name}"
+
+  vpc_id     = "${module.vpc.vpc_id}"
+  subnet_ids = "${module.vpc.worker_subnet_ids}"
+  sg_ids     = "${concat(var.tectonic_aws_worker_extra_sg_ids, list(module.vpc.worker_sg_id))}"
+
+  ssh_key                      = "${var.tectonic_aws_ssh_key}"
+  cl_channel                   = "${var.tectonic_cl_channel}"
+  user_data                    = "${module.ignition_workers.ignition}"
+  cluster_id                   = "${module.tectonic.cluster_id}"
+  extra_tags                   = "${var.tectonic_aws_extra_tags}"
+  autoscaling_group_extra_tags = "${var.tectonic_autoscaling_group_extra_tags}"
+
+  root_volume_type = "${var.tectonic_aws_worker_root_volume_type}"
+  root_volume_size = "${var.tectonic_aws_worker_root_volume_size}"
+  root_volume_iops = "${var.tectonic_aws_worker_root_volume_iops}"
+}
---
+++ tectonic.tf
@@ -0,0 +1,146 @@
+module "bootkube" {
+  source         = "github.com/coreos/tectonic-installer//modules/bootkube?ref=1.7.3-tectonic.2"
+  cloud_provider = "aws"
+
+  cluster_name = "${var.tectonic_cluster_name}"
+
+  kube_apiserver_url = "https://${module.masters.api_internal_fqdn}:443"
+  oidc_issuer_url    = "https://${module.masters.ingress_internal_fqdn}/identity"
+
+  # Platform-independent variables wiring, do not modify.
+  container_images = "${var.tectonic_container_images}"
+  versions         = "${var.tectonic_versions}"
+
+  ca_cert    = "${var.tectonic_ca_cert}"
+  ca_key     = "${var.tectonic_ca_key}"
+  ca_key_alg = "${var.tectonic_ca_key_alg}"
+
+  service_cidr = "${var.tectonic_service_cidr}"
+  cluster_cidr = "${var.tectonic_cluster_cidr}"
+
+  advertise_address = "0.0.0.0"
+  anonymous_auth    = "false"
+
+  oidc_username_claim = "email"
+  oidc_groups_claim   = "groups"
+  oidc_client_id      = "tectonic-kubectl"
+
+  etcd_endpoints   = "${module.etcd.endpoints}"
+  etcd_ca_cert     = "${var.tectonic_etcd_ca_cert_path}"
+  etcd_client_cert = "${var.tectonic_etcd_client_cert_path}"
+  etcd_client_key  = "${var.tectonic_etcd_client_key_path}"
+  etcd_tls_enabled = "${var.tectonic_etcd_tls_enabled}"
+
+  etcd_cert_dns_names = [
+    "${var.tectonic_cluster_name}-etcd-0.${var.tectonic_base_domain}",
+    "${var.tectonic_cluster_name}-etcd-1.${var.tectonic_base_domain}",
+    "${var.tectonic_cluster_name}-etcd-2.${var.tectonic_base_domain}",
+    "${var.tectonic_cluster_name}-etcd-3.${var.tectonic_base_domain}",
+    "${var.tectonic_cluster_name}-etcd-4.${var.tectonic_base_domain}",
+    "${var.tectonic_cluster_name}-etcd-5.${var.tectonic_base_domain}",
+    "${var.tectonic_cluster_name}-etcd-6.${var.tectonic_base_domain}",
+  ]
+
+  experimental_enabled = "${var.tectonic_experimental}"
+
+  master_count = "${var.tectonic_master_count}"
+
+  # The default behavior of Kubernetes's controller manager is to mark a node
+  # as Unhealthy after 40s without an update from the node's kubelet. However,
+  # AWS ELB's Route53 records have a fixed TTL of 60s. Therefore, when an ELB's
+  # node disappears (e.g. scaled down or crashed), kubelet might fail to report
+  # for a period of time that exceed the default grace period of 40s and the
+  # node might become Unhealthy. While the eviction process won't start until
+  # the pod_eviction_timeout is reached, 5min by default, certain operators
+  # might already have taken action. This is the case for the etcd operator as
+  # of v0.3.3, which removes the likely-healthy etcd pods from the the
+  # cluster, potentially leading to a loss-of-quorum as generally all kubelets
+  # are affected simultaneously.
+  #
+  # To cope with this issue, we increase the grace period, and reduce the
+  # pod eviction time-out accordingly so pods still get evicted after an total
+  # time of 340s after the first post-status failure.
+  #
+  # Ref: https://github.com/kubernetes/kubernetes/issues/41916
+  # Ref: https://github.com/kubernetes-incubator/kube-aws/issues/598
+  node_monitor_grace_period = "2m"
+
+  pod_eviction_timeout = "220s"
+}
+
+module "tectonic" {
+  source   = "github.com/coreos/tectonic-installer//modules/tectonic?ref=1.7.3-tectonic.2"
+  platform = "aws"
+
+  cluster_name = "${var.tectonic_cluster_name}"
+
+  base_address       = "${module.masters.ingress_internal_fqdn}"
+  kube_apiserver_url = "https://${module.masters.api_internal_fqdn}:443"
+
+  # Platform-independent variables wiring, do not modify.
+  container_images = "${var.tectonic_container_images}"
+  versions         = "${var.tectonic_versions}"
+
+  license_path     = "${var.tectonic_vanilla_k8s ? "/dev/null" : pathexpand(var.tectonic_license_path)}"
+  pull_secret_path = "${var.tectonic_vanilla_k8s ? "/dev/null" : pathexpand(var.tectonic_pull_secret_path)}"
+
+  admin_email         = "${var.tectonic_admin_email}"
+  admin_password_hash = "${var.tectonic_admin_password_hash}"
+
+  update_channel = "${var.tectonic_update_channel}"
+  update_app_id  = "${var.tectonic_update_app_id}"
+  update_server  = "${var.tectonic_update_server}"
+
+  ca_generated = "${module.bootkube.ca_cert == "" ? false : true}"
+  ca_cert      = "${module.bootkube.ca_cert}"
+  ca_key_alg   = "${module.bootkube.ca_key_alg}"
+  ca_key       = "${module.bootkube.ca_key}"
+
+  console_client_id = "tectonic-console"
+  kubectl_client_id = "tectonic-kubectl"
+  ingress_kind      = "NodePort"
+  experimental      = "${var.tectonic_experimental}"
+  master_count      = "${var.tectonic_master_count}"
+  stats_url         = "${var.tectonic_stats_url}"
+
+  image_re = "${var.tectonic_image_re}"
+}
+
+module "flannel-vxlan" {
+  source = "github.com/coreos/tectonic-installer//modules/net/flannel-vxlan?ref=1.7.3-tectonic.2"
+
+  flannel_image     = "${var.tectonic_container_images["flannel"]}"
+  flannel_cni_image = "${var.tectonic_container_images["flannel_cni"]}"
+  cluster_cidr      = "${var.tectonic_cluster_cidr}"
+
+  bootkube_id = "${module.bootkube.id}"
+}
+
+module "calico-network-policy" {
+  source = "github.com/coreos/tectonic-installer//modules/net/calico-network-policy?ref=1.7.3-tectonic.2"
+
+  kube_apiserver_url = "https://${module.masters.api_internal_fqdn}:443"
+  calico_image       = "${var.tectonic_container_images["calico"]}"
+  calico_cni_image   = "${var.tectonic_container_images["calico_cni"]}"
+  cluster_cidr       = "${var.tectonic_cluster_cidr}"
+  enabled            = "${var.tectonic_calico_network_policy}"
+
+  bootkube_id = "${module.bootkube.id}"
+}
+
+data "archive_file" "assets" {
+  type       = "zip"
+  source_dir = "./generated/"
+
+  # Because the archive_file provider is a data source, depends_on can't be
+  # used to guarantee that the tectonic/bootkube modules have generated
+  # all the assets on disk before trying to archive them. Instead, we use their
+  # ID outputs, that are only computed once the assets have actually been
+  # written to disk. We re-hash the IDs (or dedicated module outputs, like module.bootkube.content_hash)
+  # to make the filename shorter, since there is no security nor collision risk anyways.
+  #
+  # Additionally, data sources do not support managing any lifecycle whatsoever,
+  # and therefore, the archive is never deleted. To avoid cluttering the module
+  # folder, we write it in the Terraform managed hidden folder `.terraform`.
+  output_path = "./.terraform/generated_${sha1("${module.tectonic.id} ${module.bootkube.id} ${module.flannel-vxlan.id} ${module.calico-network-policy.id}")}.zip"
+}
